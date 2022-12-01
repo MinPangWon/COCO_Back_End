672024aabd8945a10462230a595705754df16b07
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2022-12-01 22:56:31 +0900

    <Feat> : Board
    <Feat> : Board
    -----------------------------------------
    1. Board to React
    2. Database Change
    
    -----------------------------------------
    <>

--- app/api/routers/board.py
+++ app/api/routers/board.py
@@ -4,16 +4,28 @@ from crud.board import CrudBoard
 
 router = APIRouter()
 
+crudBoard = CrudBoard()
+
 class Board(BaseModel):
     id: int
+    context: str
     title: str
-    user_id: str
+    rel_task: int
     time: str
     category: int
     likes: int
     views: int
     comments: int
 
+#글쓴이
+class Writer(BaseModel):
+    user_id: str
+
+
 @router.get('/board/', tags = ['board'])
 async def check_board():
-    return CrudBoard.check_board()
+    return crudBoard.check_board()
+
+@router.post('/board/{board_id}', tags=['board'])
+async def board_detail(board_id: int):
+    return crudBoard.board_detail(board_id)
